c34143ee13774905ff97f7e1eb3bbdf9944a0e8b
Atila <atilafb77@gmail.com>
2022-01-18 12:19:42 -0300

    creating log_parser

---
+++ log_parser.rb
@@ -0,0 +1,25 @@
+class LogParser
+    def initialize(args)
+      if args.first.nil?
+        @file_name = "games.log"
+      else
+        @file_name = args.first
+      end
+    end
+  
+    def process_file (file)
+        file_data = file.readlines.map(&:chomp)
+        puts file_data.first
+    end
+
+    def validate
+        begin
+            file = File.open(@file_name)
+            process_file(file)
+        rescue
+            puts 'File not exists.'
+        ensure
+            file.close unless file.nil?
+        end         
+    end
+  end
--- main.rb
+++ main.rb
@@ -1,28 +1,3 @@
-class Main
-    def initialize(args)
-      if args.first.nil?
-        @file_name = "games.log"
-      else
-        @file_name = args.first
-      end
-    end
-  
-    def process_file (file)
-        file_data = file.readlines.map(&:chomp)
-        puts file_data.first
-    end
+require_relative "log_parser"
 
-    def validate
-        begin
-            file = File.open(@file_name)
-            process_file(file)
-        rescue
-            puts 'File not exists.'
-        ensure
-            file.close unless file.nil?
-        end         
-    end
-
-  end
-  
-  Main.new(ARGV).validate if $PROGRAM_NAME == __FILE__
+LogParser.new(ARGV).validate if $PROGRAM_NAME == __FILE__
