f9ee6a36cf2fd1991ccfecdb57ce219ca1ef2577
Atila <atilafb77@gmail.com>
2022-01-19 14:30:46 -0300

    fix: fixing class indentation

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,26 +1,26 @@
-  class LogParser
-    def initialize(file_path = "games.log")
-      @file_name = file_path
-    end
-
-    def execute
-      process_file(validate_file) unless validate_file.nil?
-    end
-
-  private
-    def process_file (file)
-        file_data = file.readline.chomp
-        puts file_data
-        file.close
-        file_data
-    end
-
-    def validate_file
-        begin
-            File.open(@file_name)
-        rescue
-            puts 'File does not exists'
-            nil 
-        end         
-    end
+class LogParser
+  def initialize(file_path = "games.log")
+    @file_name = file_path
   end
+
+  def execute
+    process_file(validate_file) unless validate_file.nil?
+  end
+
+private
+  def process_file (file)
+    file_data = file.readline.chomp
+    puts file_data
+    file.close
+    file_data
+  end
+
+  def validate_file
+    begin   
+      File.open(@file_name)
+    rescue
+      puts 'File does not exists'
+      nil 
+    end         
+  end
+end
