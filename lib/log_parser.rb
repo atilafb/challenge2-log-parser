7d85b4bcae5e3e0ad3ed44e474b336abe71744f9
Atila <atilafb77@gmail.com>
2022-01-19 15:57:34 -0300

    fix: refactoring code to use ensure to prevent file not being closed.

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -3,24 +3,22 @@ class LogParser
     @file_name = file_path
   end
 
-  def execute
-    process_file(validate_file) unless validate_file.nil?
+  def validate_file
+    begin   
+      file = File.open(@file_name)
+      process_file(file)
+    rescue
+      puts 'File does not exists'
+      nil
+    ensure
+      file.close unless file.nil?
+    end    
   end
 
 private
   def process_file(file)
     file_data = file.readline.chomp
     puts file_data
-    file.close
     file_data
-  end
-
-  def validate_file
-    begin   
-      File.open(@file_name)
-    rescue
-      puts 'File does not exists'
-      nil 
-    end         
   end
 end
--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -1,15 +1,15 @@
 require_relative "../../lib/log_parser.rb"
 
-describe '#execute' do
-  it 'execute the methods to run the file' do
+describe '#validate_file' do
+  it 'validate the methods to run the file' do
     log = LogParser.new('./spec/fixtures/game_test.log')
-    log_parse = log.execute
-    expect(log_parse). to eq('  0:00 ------------------------------------------------------------')
+    log_parse = log.validate_file
+    expect(log_parse).to eq('  0:00 ------------------------------------------------------------')
   end
 
-  it 'not execute the methods if the file does not exists' do
+  it 'not validate the methods if the file does not exists' do
     log = LogParser.new("outro_arquivo.log")
-    log_parse = log.execute
+    log_parse = log.validate_file
     expect(log_parse).to be_falsey
   end
 end
