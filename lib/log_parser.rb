fc75241161a617fc097be43406a47e4cbb736259
Atila <atilafb77@gmail.com>
2022-01-24 19:33:11 -0300

    feat: add find_players method to log_parser

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -17,13 +17,35 @@ class LogParser
   def count_lines
     file = File.open(@file_name)
     lines = File.read(file).each_line.count
-    puts lines
     file.close
     lines
   end
 
+  def find_players
+    players = []
+    file = File.open(@file_name)
+    file.each do |line|
+      if line.include?('Kill')
+        player = get_player_from_line(line, 'killed ', ' by')
+        players.push(player) unless players.include?(player)
+      elsif line.include?('ClientUserinfoChanged')
+        player = get_player_from_line(line, ' n\\', '\\t')
+        players.push(player) unless players.include?(player)
+      end
+    end
+    file.close
+    players
+  end
+
+  def get_player_from_line(line, separator1, separator2)
+    separator1_line = line.split(separator1, -1)
+    line_separated = separator1_line[1]
+    separator2_line = line_separated.split(separator2, -1)
+    separator2_line[0]
+  end
+
   def process_file
-    obj = { "lines": count_lines }
+    obj = { "lines": count_lines , "players": find_players}
     json = JSON.pretty_generate(obj)
     response = "\"#{@file_name}\": #{json}"
     puts response
--- main.rb
+++ main.rb
@@ -1,6 +1,4 @@
 require_relative "./lib/log_parser.rb"
 
 log_parse = LogParser.new
-log_parse.first_line
-log_parse.count_lines
 log_parse.process_file
